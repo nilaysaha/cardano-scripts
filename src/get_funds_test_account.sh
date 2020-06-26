a5711c4e62969d7ea8a99caca03d4ebc7691715c
nilay saha <saha.nilay@gmail.com>
2020-06-26 05:36:57 +0200

    new fund address

--- src/get_funds_test_account.sh
+++ src/get_funds_test_account.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PAYMENT_ADDR=`cat ./kaddr/payment.addr`
-curl -v -XPOST "https://faucet.ff.dev.cardano.org/send-money/$PAYMENT_ADDR"
+curl -v -XPOST "https://faucet.shelley-testnet.dev.cardano.org/send-money/$PAYMENT_ADDR"            
