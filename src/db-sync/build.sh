ce0d31d85e66df73a70529c647c047cf1de760da
nilay saha <saha.nilay@gmail.com>
2020-11-01 19:46:23 +0100

    latest changes for db-sync

--- src/db-sync/build.sh
+++ src/db-sync/build.sh
@@ -5,13 +5,13 @@ cd ${DB_SYNC_DIR}
 
 
 #nix-build -A cardano-db-sync -o db-sync-node
-/scripts/postgresql-setup.sh --createdb
 
 SOCKET_DIR="../cardano-scripts/exec/state-node-shelly-mainnet/node.socket"  #relative to cardano-db-sync as we are now thre.
+LEDGER_STATE_DIR="../cardano-scripts/exec/state-node-shelly-mainnet/db-ff/ledger/"
+#PGPASSFILE=config/pgpass-mainnet scripts/postgresql-setup.sh --createdb
 
-PGPASSFILE=config/pgpass-mainnet
-
-db-sync-node/bin/cardano-db-sync \
+PGPASSFILE=config/pgpass-mainnet db-sync-node/bin/cardano-db-sync \
     --config config/mainnet-config.yaml \
     --socket-path ${SOCKET_DIR} \
-    --schema-dir schema/
+    --state-dir ${LEDGER_STATE_DIR} \
+    --schema-dir ./schema/
