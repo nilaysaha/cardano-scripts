69b527decf77bfbc6f0a28f00caaa3be75bcb02b
Lkbh pool <saha.nilay@gmail.com>
2021-05-24 16:46:24 +0200

    working version of the slot leader calculation. Relies on cloning https://github.com/DamjanOstrelic/cardano-leader-logs

--- src/cscripts/myleaderlog.sh
+++ src/cscripts/myleaderlog.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-
-SLOTLEADER_CONFIG="./slotLeaderconfig.json"
+SLOTLEADER_CONFIG="$HOME/projects/cardano-scripts/src/cscripts/slotLeaderconfig.json"
 LEDGER_FILE="/tmp/ledger.json"
+VRF_KEY_FILE="$HOME/projects/cardano-scripts/src/kaddr_run/vrf.skey"
+CARDANO_LLOG_EXEC="$HOME/projects/cardano-leader-logs/"
 cardano-cli query ledger-state --mainnet --out-file $LEDGER_FILE
 EPOCH_NONCE=$(cardano-cli query protocol-state --mainnet | jq -r .csTickn.ticknStateEpochNonce.contents)
-node cardanoLeaderLogs.js path/to/slotLeaderLogsConfig.json $EPOCH_NONCE
+cd $CARDANO_LLOG_EXEC
+node  cardanoLeaderLogs.js $CARDANO_LLOG_EXEC  $SLOTLEADER_CONFIG  $EPOCH_NONCE
