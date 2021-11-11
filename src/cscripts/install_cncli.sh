774cbf820e454d1823fe872c9c89c53c8d38c016
Nilay saha <saha.nilay@gmail.com>
2021-11-11 22:23:40 +0100

    added cncli install script

---
+++ src/cscripts/install_cncli.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION_ID=$1
+
+
+if [ $# -ne 1 ]; then
+    echo "The number of arguments provided is incorrect. Please provide the release version to install"
+    echo "Example: ./install_cncli.shs 4.0.1 (skip the v infront of the release)"
+    exit 1
+fi
+
+curl -sLJ https://github.com/AndrewWestberg/cncli/releases/download/v$VERSION_ID/cncli-$VERSION_ID-x86_64-unknown-linux-gnu.tar.gz -o /tmp/cncli-$VERSION_ID-x86_64-unknown-linux-gnu.tar.gz
