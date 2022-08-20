4ee16a3e676ceafac1b4c15ecbb66de434376486
Nilay saha <saha.nilay@gmail.com>
2022-08-21 01:59:30 +0200

    updated script for local lib installation

--- exec/install_local_libs.sh
+++ exec/install_local_libs.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+#install pre-req
+sudo apt install build-essential
+sudo apt-get install libtool
+sudo apt-get install autoconf
+
+sudo apt-get install -y \
+	git automake build-essential pkg-config \
+	libffi-dev libgmp-dev libssl-dev \
+	libtinfo-dev libsystemd-dev \
+	zlib1g-dev make g++ tmux git \
+	jq wget libncursesw5 \
+	libtool autoconf curl \
+        awscli
+    
+git clone https://github.com/input-output-hk/libsodium; \
+	cd libsodium; \
+	git checkout 66f017f1; \
+	./autogen.sh; \
+	./configure; \
+	make; \
+	make install;
+
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+export  PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"	
+
 git clone https://github.com/bitcoin-core/secp256k1.git; \
         cd secp256k1; \  
         git reset --hard ac83be33d0956faf6b7f61a60ab524ef7d6a473a ; \
@@ -8,3 +33,6 @@ git clone https://github.com/bitcoin-core/secp256k1.git; \
         make; \
         make check; \
         sudo make install;
+
+#cleanup
+rm -rf libsodium secp256k1	
