2c01e35ad25d563492286abb8b13325e1b08da99
Nilay Saha <saha.nilay@gmail.com>
2024-01-27 23:31:59 +0100

    testnet separated for cardano wallet scritps

--- exec/start_cw_local_node.sh
+++ exec/start_cw_local_node.sh
@@ -5,4 +5,4 @@ CONFIG_FILE="$HOME/projects/cardano-scripts/src/tconfig/mainnet/byron-genesis.js
 DB_DIR="$HOME/wallets/db"
 SOCKET="$HOME/projects/cardano-scripts/exec/cardano-chain-data-store/node.socket"
 
-$CARDANO_WALLET serve --port 8090 --testnet $CONFIG_FILE --database $DB_DIR --node-socket $SOCKET --token-metadata-server https://tokens.cardano.org --trace-wallet-db=debug 
+$CARDANO_WALLET serve --port 8090 --mainnet --database $DB_DIR --node-socket $SOCKET --token-metadata-server https://tokens.cardano.org --trace-wallet-db=debug 
