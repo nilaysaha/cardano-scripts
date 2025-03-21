a70871acd26eadadbb8a0fd4a01316d2df7312cb
Nilay Saha <saha.nilay@gmail.com>
2025-03-21 16:26:25 +0100

    added preview files

--- exec/start_cw_local_node_preview.sh
+++ exec/start_cw_local_node_preview.sh
@@ -3,6 +3,6 @@
 CARDANO_WALLET="/home/nsaha/.cabal/bin/cardano-wallet"
 CONFIG_FILE="$HOME/projects/cardano-scripts/src/tconfig/preview/byron-genesis.json"
 DB_DIR="$HOME/wallets/db"
-SOCKET="$HOME/projects/cardano-scripts/exec/cardano-chain-data-store/node.socket"
+SOCKET="/home/developers/node.socket"
 
 $CARDANO_WALLET serve --port 8091 --testnet $CONFIG_FILE --database $DB_DIR --node-socket $SOCKET --token-metadata-server https://tokens.cardano.org --trace-wallet-db=debug 
